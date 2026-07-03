d8b91437f1f9fe2683b0f3ad263cae176de684e8
Harish Bodasu <146125728+HarishB301@users.noreply.github.com>
2026-07-03 18:54:34 +0530

    Time: 2 ms (96.13%), Space: 45.9 MB (33.53%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,22 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        sum(nums,list,new ArrayList<>(),0,target);
+    public List<List<Integer>> combinationSum(int[] can, int target) {
+        List<List<Integer>> list=new ArrayList<>();
+        sum(list,new ArrayList<>(),can,target,0);
         return list;
     }
 
-    public void sum(int[] nums,List<List<Integer>>list,List<Integer> res,int index,int target){
-            if(target==0){
-                list.add(new ArrayList<>(res));    
-            }
-            else if(target<0) return;
-            else{
-                 for(int i=index;i<nums.length;i++){
-                res.add(nums[i]);
-                sum(nums,list,res,i,target-nums[i]);
+    public void sum(List<List<Integer>> list,List<Integer> res,int[] can,int target,int index){
+        if(target==0){         
+            list.add(new ArrayList<>(res));    
+            return;
+        }
+        else if(target<0) return;
+        else{
+            for(int i=index;i<can.length;i++){
+                res.add(can[i]);
+                sum(list,res,can,target-can[i],i);
                 res.remove(res.size()-1);
-                }
             }
+        }
     }
 }
