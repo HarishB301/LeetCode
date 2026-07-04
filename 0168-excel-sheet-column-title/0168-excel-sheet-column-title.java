5549ac4825677c081dbaa730de1d4133260381d2
Harish Bodasu <146125728+HarishB301@users.noreply.github.com>
2026-07-04 20:20:49 +0530

    Time: 0 ms (100%), Space: 42.6 MB (22.41%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder str = new StringBuilder();
+        while(n>0){
+            n--;
+            str.append((char)('A'+(n%26)));
+            n=n/26;
+
+        }
+
+        return str.reverse().toString();
+    }
+}
