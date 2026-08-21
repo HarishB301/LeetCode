01edff3dfa9ff87542205e6cac960224adf318d1
Harish Bodasu <146125728+HarishB301@users.noreply.github.com>
2026-08-21 23:13:34 +0530

    Time: 122 ms (10.28%), Space: 48.7 MB (9.71%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -3,9 +3,8 @@ class Solution {
         Stack<Character> str=new Stack<>();
         String Adj="";
         for(char ch:s.toCharArray()){
-            if(str.isEmpty()) str.push(ch);
-            else if(str.peek()!=ch) str.push(ch);
-            else str.pop();
+            if(!str.isEmpty() && str.peek()==ch) str.pop();
+            else str.push(ch);
         }
         for(char ch:str) Adj+=ch;
         return Adj;
